625bf84812b1f76477383f2caa1c07640818f53b
gitJanaeW <janae.wel@gmail.com>
2022-12-09 12:13:58 -0500

    base app host test

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {Suspense, lazy} from "react";
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Nav from "./components/Nav";
+const About = lazy(() => import('./components/About'));
+// const Portfolio = lazy(() => import('./components/Portfolio'));
+// const Contact = lazy(() => import('./components/Contact'));
+// const Resume =  lazy(() => import('./components/Resume'));
+const Footer = lazy(() => import("./components/Footer"));
+
+const App = () => {
+    return(
+        <BrowserRouter>
+            <Nav/>
+                <Suspense fallback={<div style={{minHeight:"10vh"}}></div>}>
+                    <Routes>
+                        <Route path="/" element={<About/>}/>
+                        {/* <Route path="/portfolio" element={<Portfolio/>}/> */}
+                        {/* <Route path="/contact" element={<Contact/>}/> */}
+                        {/* <Route path="/resume" element={<Resume/>}/> */}
+                    </Routes>
+                </Suspense>
+            <Footer/>
+        </BrowserRouter>
+    );
+}
+export default App;
---
+++ src/components/Nav.js
@@ -0,0 +1,48 @@
+import {useState} from "react";
+import {Link} from "react-router-dom";
+
+const Nav = () => {
+    const [clickedAbout, setAbout] = useState(true);
+    const [clickedPortfolio, setPortfolio] = useState(false);
+    const [clickedContact, setContact] = useState(false);
+    const [clickedResume, setResume] = useState(false);
+    const highlight = () => {
+        const path = window.location.pathname;
+        setAbout(path === "/");
+        setPortfolio(path === "/portfolio");
+        setContact(path === "/contact");
+        setResume(path ==="/resume");
+    };
+    return(
+        <nav onClick={highlight} className="navbar navbar-expand-lg navbar-light px-4 py-4">
+            <Link to="/">
+                <h1 className="navbar-brand font-weight-bold"><strong>JANAE WELSH</strong></h1>
+            </Link>
+            <div className="navbar-toggler collapsed px-0" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <button className="navbar-toggler navbar-burger px-0" id="burger" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+            </div>
+            <div className="container px-0 mx-0" id="flexBar"> 
+                <div className="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+                    <div className="navbar-nav" id="navbar-links">
+                        <Link to="/">
+                            <h2 className={`${clickedAbout && "bg-info bg-gradient"} nav-link fw-bold px-2`}>ABOUT ME</h2>
+                        </Link>
+                        <Link to="/portfolio">
+                            <h2 className={`${clickedPortfolio && "bg-info bg-gradient"} nav-link fw-bold px-2`}>PORTFOLIO</h2>
+                        </Link>
+                        <Link to="/contact">
+                            <h2 className={`${clickedContact && "bg-info bg-gradient"} nav-link fw-bold px-2`}>CONTACT</h2>
+                        </Link>
+                        <Link to="/resume">
+                            <h2 className={`${clickedResume && "bg-info bg-gradient"} nav-link fw-bold px-2 ml-2`}>RESUME</h2>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Nav;
